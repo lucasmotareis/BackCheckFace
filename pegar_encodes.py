354361cbc564b54efd434a6bfd3ebde3815e5bc3
Lucas Mota Reis <lucasmotareis@gmail.com>
2025-08-14 10:46:43 -0300

    Agora a API consegue ver a lista de arquivos (estilo Keep Notes) e manda o rosto encontrado para o FrontEnd

--- pegar_encodes.py
+++ pegar_encodes.py
@@ -19,30 +19,20 @@ def load_rgb(path, use_cv2=False):
             raise FileNotFoundError(f"Arquivo não encontrado: {path}")
         return img
 
-caminho = Path(r"C:\Users\lucas\Downloads\WhatsApp Unknown 2025-08-07 at 11.04.37\facialRecognition")
+caminho = Path(r"C:\Users\lucas\Downloads\ProjetoFacial\BackCheckFace\MALODROMO")
 pastas = [p for p in caminho.iterdir() if p.is_dir()]
 
 
 
 for pasta in pastas:
     contador = 1
-    imagens_jpeg = [f.name for f in pasta.iterdir() if f.suffix.lower() == ".jpeg"]
+    imagens_png = [f.name for f in pasta.iterdir() if f.suffix.lower() == ".png"]
     print(f"📂 Pasta: {pasta.name}")        
-    arquivos_json = [f for f in pasta.iterdir() if f.suffix.lower() == ".json"]
-    if not arquivos_json:
-        print(f"Nenhum JSON encontrado em {pasta.name}")
-        continue
-    with open(arquivos_json[0], "r", encoding="utf-8") as arquivo:
-        dados = json.load(arquivo)
-        nome_individuo = dados.get("nome", "Desconhecido")
-
-
-    for imagem in imagens_jpeg:
+    for imagem in imagens_png:
         p1 = os.path.join(pasta, imagem)  # monta o caminho completo
         img1 = load_rgb(p1, use_cv2=False)
         # detectar faces (tente model='cnn' se hog falhar e você tiver dlib com suporte)
         locs1 = fr.face_locations(img1,model='cnn')  
-
         if not locs1:
             print("Nenhuma face detectada em img1 — verifique rotação/recorte/qualidade.")
         # Extrair encodings com segurança
@@ -51,11 +41,10 @@ for pasta in pastas:
         if not enc1:
             print("Não foi possível extrair encoding de uma das imagens. Abortando comparação.")
         else:
-            nome_malandro = os.path.join(pasta, f"{nome_individuo}.npy")
+            nome_malandro = os.path.join(pasta, f"{pasta.name}.npy")
 
             while os.path.exists(nome_malandro):
-                nome_malandro = os.path.join(pasta,f"{nome_individuo}_{contador}.npy")
-
+                nome_malandro = os.path.join(pasta,f"{pasta.name}_{contador}.npy")
                 contador += 1
             e1 = enc1[0]
             np.save(nome_malandro, e1)
